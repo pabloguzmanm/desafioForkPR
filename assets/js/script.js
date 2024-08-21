e7230d51028f1a9b95a8bd25f93bffa8a3381089
Pablo Guzmán <pablocorporativa@gmail.com>
2024-08-20 21:29:19 -0400

    correción del js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,34 @@ form.addEventListener("submit", function (event) {
   event.preventDefault(); // Previne comportamiento por defecto
 
   // Valores ingresados por el usuario
-  const email = event.target.variableEmail.value;
-  const password = event.target.variablePassword.value;
-  const check = event.target.variableCheck.value;
+  const nombrePaciente = document.getElementById("nombrePaciente").value;
+  const motivoConsulta = document.getElementById("motivoConsulta").value;
+  const fechaConsulta = document.getElementById("fechaConsulta").value;
 
-  if (password == "") {
-    alert("Debes llenar el campo Password");
-    event.target.variablePassword.focus();
+  if (nombrePaciente == "") {
+    alert("Debes llenar el campo Nombre");
+    event.target.nombrePaciente.focus();
   }
 
-  //mostrar datos en "#contenidos" ingresados en el formulario 
-  let contenido = document.querySelector('#contenido')
+  if (motivoConsulta == "") {
+    alert("Debes llenar el campo Motivo de Consulta");
+    event.target.motivoConsulta.focus();
+  }
+
+  if (fechaConsulta == "") {
+    alert("Debes llenar el campo Fecha de Consulta");
+    event.target.fechaConsulta.focus();
+  }
+
+  //mostrar datos en "#contenidos" ingresados en el formulario
+  let contenido = document.querySelector("#contenido");
   contenido.innerHTML = `
       <hr>
       <br><strong>Nombre paciente: </strong> ${nombrePaciente}
       <br><strong>Motivo Consulta: </strong> ${motivoConsulta}
       <br><strong>Fecha Consulta: </strong> ${fechaConsulta}
-      `
+      `;
 
   //limpiar formulario
-  formulario.reset()
+  formulario.reset();
 });
